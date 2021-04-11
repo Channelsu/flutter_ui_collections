389172270a11faf14c0a1a36a37ada6bd7ebf75b
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-04-11 17:17:18 +0900

    UIサンプル一覧画面追加

---
+++ lib/screens/ui_sample_list_screen.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+import 'package:flutteruicollections/models/panel.dart';
+
+class UiSampleListScreen extends StatefulWidget {
+  @override
+  _UiSampleListScreenState createState() => _UiSampleListScreenState();
+}
+
+class _UiSampleListScreenState extends State<UiSampleListScreen> {
+
+  List<Panel> _panels = [
+    Panel(title: 'Buttons', body: 'body1'),
+    Panel(title: 'Layouts', body: 'body2'),
+    Panel(title: 'Animations', body: 'body3'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('UI Collections'),
+        backgroundColor: Colors.lightBlue,
+        leading: IconButton(icon: Icon(Icons.menu), onPressed: null),
+        actions: <Widget>[
+          Icon(Icons.settings)
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: ListView(
+          children: <Widget>[
+            Card(
+              color: Colors.lightBlue[100],
+              child: ExpansionTile(
+                title: Text('ボタン'),
+                children: <Widget>[
+                  InkWell(
+                    onTap: (){},
+                    child: ListTile(
+                      title: Text('    FlatButton'),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: (){},
+                    child: ListTile(
+                      title: Text('    FlatButton'),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: (){},
+                    child: ListTile(
+                      title: Text('    FlatButton'),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: (){},
+                    child: ListTile(
+                      title: Text('    FlatButton'),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: (){},
+                    child: ListTile(
+                      title: Text('    FlatButton'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Card(
+              color: Colors.lightBlue[100],
+              child: ExpansionTile(
+                title: Text('レイアウト'),
+                children: <Widget>[
+                  InkWell(
+                    onTap: (){},
+                    child: ListTile(
+                      title: Text('    FlatButton'),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: (){},
+                    child: ListTile(
+                      title: Text('    FlatButton'),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: (){},
+                    child: ListTile(
+                      title: Text('    FlatButton'),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: (){},
+                    child: ListTile(
+                      title: Text('    FlatButton'),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: (){},
+                    child: ListTile(
+                      title: Text('    FlatButton'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Card(
+              color: Colors.lightBlue[100],
+              child: ExpansionTile(
+                title: Text('アニメーション'),
+                children: <Widget>[
+                  InkWell(
+                    onTap: (){},
+                    child: ListTile(
+                      title: Text('    FlatButton'),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: (){},
+                    child: ListTile(
+                      title: Text('    FlatButton'),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: (){},
+                    child: ListTile(
+                      title: Text('    FlatButton'),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: (){},
+                    child: ListTile(
+                      title: Text('    FlatButton'),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: (){},
+                    child: ListTile(
+                      title: Text('    FlatButton'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
